06ae49fa62782829eb84b4c47b395d8fa1de9fc7
Sean-Jay-M <91853278+Sean-Jay-M@users.noreply.github.com>
2022-01-06 23:08:32 +0000

    Push II: Dropdown functioning.

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ function fetchData(){
   fetchData()
   year()
 
-var newapi = 'https://api.nasa.gov/planetary/apod?api_key=GDE3gez5LI92lZk0h8UxWyJVHTz6XyD1ta6OdMlQ';
+
 
 function year(){
     const listDate = [];
     const startDate ='1995-06-16';
     var today = new Date();
-    const endDate = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    const endDate = new Date().toISOString().slice(0, 10)
     const dateMove = new Date(startDate);
     let strDate = startDate;
 
@@ -31,12 +31,29 @@ function year(){
 
     var select = document.getElementById("selectNumber"); 
     console.log(listDate);
-    for(var i = 0; i < listDate.length; i++) {
+    for(var i = listDate.length - 1; i > 0; i--) {
         var opt = listDate[i];
         var el = document.createElement("option");
         el.textContent = opt;
         el.value = opt;
         select.appendChild(el);
-        
     }
+}
+
+function newphoto(date){
+  var newapi = 'https://api.nasa.gov/planetary/apod?api_key=GDE3gez5LI92lZk0h8UxWyJVHTz6XyD1ta6OdMlQ';
+  newapi += "&date="
+  newapi += date.value+ "&";
+  console.log(newapi);
+  try{
+    console.log("In try");
+    fetch(newapi)
+    .then(response=>response.json())
+    .then(json=>{
+      console.log(json)
+      document.getElementById("image").innerHTML += "<img src='" + json.url + "' id='day_img'>"
+    })
+  }catch(error){
+    console.log(error)
+  }
 }
